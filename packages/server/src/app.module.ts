717096b6cd8ed01ddbff42ba0c83c6d9119b54ab
daviromao <davi.romao0210@gmail.com>
2023-01-10 16:13:06 -0300

    feat: setup typeorm

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: configService.getOrThrow('DB_DIALECT') as any,
+        host: configService.getOrThrow('DB_HOST'),
+        port: +configService.getOrThrow<number>('DB_PORT'),
+        username: configService.getOrThrow('DB_USER'),
+        password: configService.getOrThrow('DB_PASS'),
+        database: configService.getOrThrow<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
