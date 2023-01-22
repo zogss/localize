a16cdd4d30ba3cc9fe77131dad48ae20c977aa89
zogss <yanlucasbranco@gmail.com>
2023-01-22 04:31:19 -0300

    feat: implement home and others screens as example

--- packages/mobile/src/routes/app.routes/index.tsx
+++ packages/mobile/src/routes/app.routes/index.tsx
@@ -1,11 +1,16 @@
+import { Feather, Ionicons, MaterialIcons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import React from 'react';
 import { ThemeColors } from '../../styles/colors';
 import { Home } from '../../views/Home';
+import { Profile } from '../../views/Profile';
+import { Tracking } from '../../views/Tracking';
 
 type AppNavigator = {
   Home: undefined;
+  Profile: undefined;
+  Tracking: undefined;
 };
 
 export type StackAppNavigator = NativeStackNavigationProp<AppNavigator>;
@@ -17,6 +22,7 @@ export const AppRoutes = () => (
     screenOptions={() => ({
       tabBarStyle: {
         position: 'absolute',
+        zIndex: 100,
         bottom: 18,
         left: 18,
         right: 18,
@@ -41,12 +47,86 @@ export const AppRoutes = () => (
     initialRouteName="Home"
   >
     <Nav.Screen
+      name="Profile"
+      component={Profile}
+      options={{
+        headerTitle: 'Profile',
+        tabBarIcon: ({ focused }) => (
+          <Ionicons
+            name="md-person-circle-outline"
+            size={60}
+            style={{
+              height: 60,
+            }}
+            color={focused ? ThemeColors.cyan_500 : ThemeColors.black}
+          />
+        ),
+        tabBarIconStyle: {
+          display: 'flex',
+          height: 60,
+          width: 60,
+        },
+        tabBarLabelStyle: {
+          display: 'none',
+        },
+        tabBarItemStyle: {
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+      }}
+    />
+    <Nav.Screen
       name="Home"
       component={Home}
       options={{
         headerTitle: 'Home',
-        tabBarIcon: () => <></>,
+        tabBarIcon: ({ focused }) => (
+          <MaterialIcons
+            name="home"
+            size={60}
+            style={{
+              height: 60,
+            }}
+            color={focused ? ThemeColors.cyan_500 : ThemeColors.black}
+          />
+        ),
         tabBarIconStyle: {
+          display: 'flex',
+          height: 60,
+          width: 60,
+        },
+        tabBarLabelStyle: {
+          display: 'none',
+        },
+        tabBarItemStyle: {
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+      }}
+    />
+    <Nav.Screen
+      name="Tracking"
+      component={Tracking}
+      options={{
+        headerTitle: 'Tracking',
+        tabBarIcon: ({ focused }) => (
+          <Feather
+            name="map-pin"
+            size={60}
+            style={{
+              height: 60,
+            }}
+            color={focused ? ThemeColors.cyan_500 : ThemeColors.black}
+          />
+        ),
+        tabBarIconStyle: {
+          display: 'flex',
+          height: 60,
+          width: 60,
+        },
+        tabBarLabelStyle: {
           display: 'none',
         },
         tabBarItemStyle: {
