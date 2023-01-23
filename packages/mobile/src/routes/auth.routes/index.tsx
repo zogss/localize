f97c2d9316c5d2223b962c1947913f6fb36db697
zogss <yanlucasbranco@gmail.com>
2023-01-23 04:34:34 -0300

    chore: change navigator

--- packages/mobile/src/routes/auth.routes/index.tsx
+++ packages/mobile/src/routes/auth.routes/index.tsx
@@ -1,5 +1,7 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationProp
+} from '@react-navigation/native-stack';
 import React from 'react';
 import { ThemeColors } from '../../styles/colors';
 import { SignIn } from '../../views/auth/SignIn';
@@ -12,23 +14,19 @@ type AuthNavigator = {
 
 export type StackAuthNavigator = NativeStackNavigationProp<AuthNavigator>;
 
-const Nav = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
 export const AuthRoutes = () => (
-  <Nav.Navigator
-    screenOptions={() => ({
+  <Stack.Navigator
+    screenOptions={{
       headerShown: false,
-      tabBarShowLabel: false,
-      tabBarStyle: {
-        display: 'none',
+      contentStyle: {
+        backgroundColor: ThemeColors.gray_900,
       },
-    })}
-    sceneContainerStyle={{
-      backgroundColor: ThemeColors.gray_900,
     }}
     initialRouteName="SignIn"
   >
-    <Nav.Screen name="SignIn" component={SignIn} />
-    <Nav.Screen name="SignUp" component={SignUp} />
-  </Nav.Navigator>
+    <Stack.Screen name="SignIn" component={SignIn} />
+    <Stack.Screen name="SignUp" component={SignUp} />
+  </Stack.Navigator>
 );
