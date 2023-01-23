6f611b93d93c9914be5ebeb51df23f900bd15ef3
zogss <yanlucasbranco@gmail.com>
2023-01-23 04:35:37 -0300

    feat: improve providers usage

--- packages/mobile/App.tsx
+++ packages/mobile/App.tsx
@@ -1,15 +1,17 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Provider } from 'react-redux';
 import Routes from './src/routes';
 import store from './src/store';
 
 const App = () => (
-  <Provider store={store}>
-    <NavigationContainer>
+  <NavigationContainer>
+    <Provider store={store}>
+      <StatusBar style="auto" />
       <Routes />
-    </NavigationContainer>
-  </Provider>
+    </Provider>
+  </NavigationContainer>
 );
 
 export default App;
