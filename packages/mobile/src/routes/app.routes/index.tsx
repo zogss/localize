aa3fc55b8514ed7e4ab0e995912b2e7ef6da95d9
zogss <yanlucasbranco@gmail.com>
2023-01-24 01:36:01 -0300

    fix: fix navigation with bottom tab

--- packages/mobile/src/routes/app.routes/index.tsx
+++ packages/mobile/src/routes/app.routes/index.tsx
@@ -1,7 +1,9 @@
 import { Feather, Ionicons, MaterialIcons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
 import { ThemeColors } from '../../styles/colors';
 import { Home } from '../../views/Home';
 import { Profile } from '../../views/Profile';
@@ -17,54 +19,71 @@ export type StackAppNavigator = NativeStackNavigationProp<AppNavigator>;
 
 const Nav = createBottomTabNavigator();
 
-export const AppRoutes = () => (
-  <Nav.Navigator
-    screenOptions={() => ({
-      tabBarStyle: {
-        backgroundColor: ThemeColors.dark,
-        borderTopColor: 'transparent',
-      },
-      headerShown: false,
-      tabBarShowLabel: false,
-      tabBarActiveTintColor: ThemeColors.cyan_500,
-      tabBarInactiveTintColor: ThemeColors.gray_200,
-      tabBarIconStyle: {
-        height: 60,
-        width: 60,
-      },
-      lazy: true,
-    })}
-    sceneContainerStyle={{
-      backgroundColor: ThemeColors.gray_200,
-    }}
-    initialRouteName="Home"
-  >
-    <Nav.Screen
-      name="Home"
-      component={Home}
-      options={{
-        tabBarIcon: ({ color }) => (
-          <MaterialIcons name="home" size={30} color={color} />
-        ),
+export const AppRoutes = () => {
+  //* hooks
+  const navigation = useNavigation<StackAppNavigator>();
+
+  //* render
+  return (
+    <Nav.Navigator
+      screenOptions={() => ({
+        tabBarStyle: {
+          backgroundColor: ThemeColors.dark,
+          borderTopColor: 'transparent',
+        },
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: ThemeColors.cyan_500,
+        tabBarInactiveTintColor: ThemeColors.gray_200,
+        tabBarIconStyle: {
+          height: 60,
+          width: 60,
+        },
+        lazy: true,
+      })}
+      sceneContainerStyle={{
+        backgroundColor: ThemeColors.gray_200,
       }}
-    />
-    <Nav.Screen
-      name="Tracking"
-      component={Tracking}
-      options={{
-        tabBarIcon: ({ color }) => (
-          <Feather name="map-pin" size={30} color={color} />
-        ),
-      }}
-    />
-    <Nav.Screen
-      name="Profile"
-      component={Profile}
-      options={{
-        tabBarIcon: ({ color }) => (
-          <Ionicons name="md-person-circle-outline" size={30} color={color} />
-        ),
-      }}
-    />
-  </Nav.Navigator>
-);
+      initialRouteName="Home"
+    >
+      <Nav.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+              <MaterialIcons name="home" size={30} color={color} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Nav.Screen
+        name="Tracking"
+        component={Tracking}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <TouchableOpacity onPress={() => navigation.navigate('Tracking')}>
+              <Feather name="map-pin" size={30} color={color} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Nav.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <TouchableOpacity>
+              <Ionicons
+                name="md-person-circle-outline"
+                size={30}
+                color={color}
+                onPress={() => navigation.navigate('Profile')}
+              />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+    </Nav.Navigator>
+  );
+};
