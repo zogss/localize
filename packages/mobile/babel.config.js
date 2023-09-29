1ba58eaadce738cc3283257923ebb19a58c4e5af
zogss <yanlucasbranco@gmail.com>
2023-09-29 01:51:21 -0300

    feat: configuring root import

--- packages/mobile/babel.config.js
+++ packages/mobile/babel.config.js
@@ -3,8 +3,25 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      '@babel/plugin-proposal-optional-chaining',
       '@babel/plugin-proposal-export-namespace-from',
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+      '@babel/plugin-transform-shorthand-properties',
+      '@babel/plugin-transform-arrow-functions',
+      '@babel/plugin-transform-optional-chaining',
+      '@babel/plugin-transform-nullish-coalescing-operator',
+      ['@babel/plugin-transform-template-literals', { loose: true }],
       'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@app': './src',
+          },
+        },
+      ],
     ],
   };
 };
