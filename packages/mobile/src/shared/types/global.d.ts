4b4a386e4fb03c5781dc6d9f13bcf76b4a005fcd
zogss <yanlucasbranco@gmail.com>
2025-01-12 04:03:22 -0300

    chore: assets moved inside src

---
+++ packages/mobile/src/shared/types/global.d.ts
@@ -0,0 +1,20 @@
+declare module '*.png' {
+  import {ImageSourcePropType} from 'react-native';
+
+  const value: ImageSourcePropType;
+  export default value;
+}
+
+declare module '*.jpg' {
+  import {ImageSourcePropType} from 'react-native';
+
+  const value: ImageSourcePropType;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  import {ImageSourcePropType} from 'react-native';
+
+  const value: ImageSourcePropType;
+  export default value;
+}
