2031ba1136fcbff92b7f31010ea2c36510795002
zogss <yanlucasbranco@gmail.com>
2025-01-12 20:42:02 -0300

    feat: phone validation improvements

--- packages/server/src/phone-token/dto/create-phone-token.dto.ts
+++ packages/server/src/phone-token/dto/create-phone-token.dto.ts
@@ -1,10 +1,17 @@
 import { IsString } from 'class-validator';
-import { IsPhoneNumber, Matches, MaxLength, MinLength } from 'class-validator';
+import {
+  IsPhoneNumber,
+  Matches,
+  MaxLength,
+  MinLength,
+  IsNotEmpty,
+} from 'class-validator';
 
 export class CreatePhoneTokenDto {
+  @IsNotEmpty({ message: 'Phone is required' })
   @IsString()
-  @MinLength(11)
-  @MaxLength(11)
+  @MinLength(11, { message: 'Phone must be at least 11 characters' })
+  @MaxLength(11, { message: 'Phone must be at most 11 characters' })
   @Matches(/^[0-9]+$/, { message: 'Invalid phone, only numbers are allowed' })
   @IsPhoneNumber('BR')
   phone: string;
